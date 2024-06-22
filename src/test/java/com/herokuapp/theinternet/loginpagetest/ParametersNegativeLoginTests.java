6299007debe40e9f6636db8cb305959d84bb7ec1
Stoyan Hristov <stoyan.hristov2a@gmail.com>
2024-06-22 12:38:46 +0300

    New class for negative tests that includes DataProvider from CSV file testing

--- src/test/java/com/herokuapp/theinternet/loginpagetest/ParametersNegativeLoginTests.java
+++ src/test/java/com/herokuapp/theinternet/loginpagetest/ParametersNegativeLoginTests.java
@@ -1,6 +1,5 @@
 package com.herokuapp.theinternet.loginpagetest;
 
-import com.herokuapp.theinternet.base.CsvDataProviders;
 import com.herokuapp.theinternet.base.TestUtilities;
 import com.herokuapp.theinternet.pages.LoginPage;
 import com.herokuapp.theinternet.pages.WelcomePage;
@@ -8,27 +7,13 @@ import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-import java.util.Map;
+public class ParametersNegativeLoginTests extends TestUtilities {
 
-public class NegativeLoginTests extends TestUtilities {
-
-    @Test(priority = 1, dataProvider = "csvReader", dataProviderClass = CsvDataProviders.class)
-    public void negativeLoginTest(Map<String, String> testData) {
+    @Parameters({"username", "password", "expectedMessage"})
+    @Test(priority = 1)
+    public void negativeLoginTest(String username, String password, String expectedErrorMessage) {
         log.info("Starting negativeTest");
 
-        //Data extraction and definition in separate variables
-
-        // always use the headers from CSV file to ensure correctness
-        // No:, username, password, expectedMessage, description
-
-        String numOfTest = testData.get("No:");
-        String username = testData.get("username");
-        String password = testData.get("password");
-        String expectedErrorMessage = testData.get("expectedMessage");
-        String description = testData.get("description");
-
-        // Log
-        log.info("Starting negativeLogInTest #:" + numOfTest + " for " + description);
         // open main page
         WelcomePage welcomePage = new WelcomePage(driver, log);
         welcomePage.openPage();
